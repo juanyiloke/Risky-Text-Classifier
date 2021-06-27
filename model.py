2a13466d3f3433f9209f6dbc1d48cc7965129a35
juanyiloke <lokejuanyi@gmail.com>
2021-06-27 17:19:39 -0400

    this shit def does not work lmao

--- model.py
+++ model.py
@@ -60,32 +60,73 @@ if __name__ == "__main__":
     print(df)
     print(df.info())
 
+    ### unary classifer
 
-    ## Split the data
+    from sklearn.model_selection import train_test_split
+    from sklearn.metrics import f1_score
+    from sklearn.svm import OneClassSVM
 
-    Train_X, Test_X, Train_Y, Test_Y = sklearn.model_selection  .train_test_split(df['text'], df['label'],
-                                                                        test_size=0.3)
+    # split into train/test sets
+    trainX, testX, trainy, testy = train_test_split(df['text'], df['label'], test_size=0.3)
+
     # encode text data
     Encoder = sklearn.preprocessing.LabelEncoder()
-    Train_Y = Encoder.fit_transform(Train_Y)
-    Test_Y = Encoder.fit_transform(Test_Y)
+    trainy = Encoder.fit_transform(trainy)
+    testy = Encoder.fit_transform(testy)
 
     # Vectorization of the words, we will use TF-IDF
 
-    Tfidf_vect = sklearn.feature_extraction.text.TfidfVectorizer(max_features=10000)
+    Tfidf_vect = sklearn.feature_extraction.text.TfidfVectorizer(max_features=5000)
     Tfidf_vect.fit(df['text'])
-    Train_X_Tfidf = Tfidf_vect.transform(Train_X)
-    Test_X_Tfidf = Tfidf_vect.transform(Test_X)
+    Train_X_Tfidf = Tfidf_vect.transform(trainX)
+    Test_X_Tfidf = Tfidf_vect.transform(testX)
 
-    print(Train_X_Tfidf)
-    print(len(Train_Y))
+    trainX = Train_X_Tfidf
+    testX = Test_X_Tfidf
+
+    # define outlier detection model
+    model = OneClassSVM(gamma='scale', nu=0.01)
+    # fit on majority class
+    trainX = trainX[trainy == 0]
+    model.fit(trainX)
+    # detect outliers in the test set
+    yhat = model.predict(testX)
+    print("SVM Accuracy Score -> ", sklearn.metrics.accuracy_score(yhat, testy) * 100)
+    # mark inliers 1, outliers -1
+    testy[testy == 1] = -1
+    testy[testy == 0] = 1
+    # calculate score
+    score = f1_score(testy, yhat, pos_label=-1)
+    print('F1 Score: %.3f' % score)
 
 
-    # Classifier - Algorithm - SVM
-    # fit the training dataset on the classifier
-    SVM = sklearn.svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
-    SVM.fit(Train_X_Tfidf, Train_Y)
-    # predict the labels on validation dataset
-    predictions_SVM = SVM.predict(Test_X_Tfidf)
-    # Use accuracy_score function to get the accuracy
-    print("SVM Accuracy Score -> ", sklearn.metrics.accuracy_score(predictions_SVM, Test_Y) * 100)
+    ### binary classifier
+
+    ## Split the data
+    #
+    # Train_X, Test_X, Train_Y, Test_Y = sklearn.model_selection  .train_test_split(df['text'], df['label'],
+    #                                                                     test_size=0.3)
+    # # encode text data
+    # Encoder = sklearn.preprocessing.LabelEncoder()
+    # Train_Y = Encoder.fit_transform(Train_Y)
+    # Test_Y = Encoder.fit_transform(Test_Y)
+    #
+    # # Vectorization of the words, we will use TF-IDF
+    #
+    # Tfidf_vect = sklearn.feature_extraction.text.TfidfVectorizer(max_features=10000)
+    # Tfidf_vect.fit(df['text'])
+    # Train_X_Tfidf = Tfidf_vect.transform(Train_X)
+    # Test_X_Tfidf = Tfidf_vect.transform(Test_X)
+    #
+    # print(Train_X_Tfidf)
+    # print(len(Train_Y))
+    #
+    #
+    # # We shall try the binary classifier first.
+    # # fit the training dataset on the classifier
+    # SVM = sklearn.svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
+    # SVM.fit(Train_X_Tfidf, Train_Y)
+    # # predict the labels on validation dataset
+    # predictions_SVM = SVM.predict(Test_X_Tfidf)
+    # # Use accuracy_score function to get the accuracy
+    # print("SVM Accuracy Score -> ", sklearn.metrics.accuracy_score(predictions_SVM, Test_Y) * 100)
